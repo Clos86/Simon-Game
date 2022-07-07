014c293b5875d4616aa127b5588085e4d59476d1
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-07 16:37:10 -0400

    added Game Over

--- index.js
+++ index.js
@@ -4,30 +4,51 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
-
 var level = 0
+
 
 $(document).keypress(function() {
   if (!started) {
-
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
-
 $('.btn').click(function() {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
   animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    
+    console.log("wrong");
+  }
+  };
+
 function nextSequence() {
-
+  userClickedPattern = [];
   level++;
-
   $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);  
