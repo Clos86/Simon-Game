c19a813fc1c2a3d334cd16d6c2a5052dd3c3bd0b
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-07-06 21:46:00 -0400

    added animation and sound

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 
-
 var gamePattern = [];
-
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);  
+
   var randomChosenColor = buttonColors[randomNumber];
+
   gamePattern.push(randomChosenColor);
-  console.log(gamePattern);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
   
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 };
 
 nextSequence();
